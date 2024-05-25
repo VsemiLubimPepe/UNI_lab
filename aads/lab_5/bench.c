0acaae8ab1fac08904e7a9ab2b98f05f1b96fce3
VsemiLubimPepe <135699362+VsemiLubimPepe@users.noreply.github.com>
2024-05-25 21:29:32 +0300

    Updated functions of matrix.c and added Makefile

--- aads/lab_5/bench.c
+++ aads/lab_5/bench.c
@@ -6,8 +6,8 @@ int main(int argc, char **argv)
     struct matrix *mtrx_a;
     struct matrix *mtrx_b;
 
-    FILE *mtrx_a_file = fopen("D:/c-programs/aads/lab_5/matrices/matrix_a", "r");
-    FILE *mtrx_b_file = fopen("D:/c-programs/aads/lab_5/matrices/matrix_b", "r");
+    FILE *mtrx_a_file = fopen("matrices/matrix_a", "r");
+    FILE *mtrx_b_file = fopen("matrices/matrix_b", "r");
     
     if ((!mtrx_a_file) || (!mtrx_b_file)) {
 	printf("Couldn't open file.\n");
@@ -22,17 +22,26 @@ int main(int argc, char **argv)
 
     fclose(mtrx_a_file);
     fclose(mtrx_b_file);
-    
-    struct matrix *mtrx_c = matrix_mul(mtrx_a, mtrx_b);
 
+    //struct matrix *mtrx_c = matrix_get_column_vector(mtrx_a, 0);
+    //struct matrix *housholder_c = matrix_housholder_vector(mtrx_c);
+
+    //struct matrix *cut_vector = matrix_get_cut_column_vector(mtrx_a, 1, 1);
+
+    printf("I am before housholder transformation\n");
+    matrix_housholder_transformation(mtrx_a);
     matrix_print(mtrx_a);
-    matrix_print(mtrx_b);
+    //matrix_print(mtrx_b);
 
-    matrix_print(mtrx_c);
+    //matrix_print(cut_vector);
     
-    matrix_free(mtrx_a);
-    matrix_free(mtrx_b);
-    matrix_free(mtrx_c);
+    //matrix_print(mtrx_c);
+    //matrix_print(housholder_c);
+    
+    //matrix_free(mtrx_a);
+    //matrix_free(mtrx_b);
+    //matrix_free(mtrx_c);
+    //matrix_free(housholder_c);
 
     return 0;
 }
--- aads/lab_5/matrix.h
+++ aads/lab_5/matrix.h
@@ -10,10 +10,34 @@ struct matrix {
     double _Complex *values;
 };
 
+/*
+ * Pair of numbers for Givens rotation,
+ * where c = cos(theta), s = sin(theta)
+ */ 
+struct givens_pair {
+    double _Complex c;
+    double _Complex s;
+};
+
 int matrix_fread(FILE *mtrx_file, struct matrix **mtrx);
 struct matrix *matrix_sum(struct matrix *mtrx_1, struct matrix *mtrx_2);
 struct matrix *matrix_dif(struct matrix *mtrx_1, struct matrix *mtrx_2);
 struct matrix *matrix_mul(struct matrix *mtrx_1, struct matrix *mtrx_2);
 struct matrix *matrix_transpose(struct matrix *mtrx);
+struct matrix *matrix_cut_matrix(struct matrix *mtrx, size_t row, size_t column);
+int matrix_paste_matrix(struct matrix *mtrx_1, struct matrix *mtrx_2, size_t row, size_t column);
+struct matrix *matrix_const_mul(struct matrix *mtrx, double _Complex number);
+struct givens_pair *givens_pair_find(double _Complex a, double _Complex b);
+struct matrix *matrix_givens_left_mul(struct matrix *mtrx, struct givens_pair *givens_pair);
+struct matrix *matrix_givens_right_mul(struct matrix *mtrx, struct givens_pair *givens_pair);
+struct matrix *matrix_get_column_vector(struct matrix *mtrx, size_t column);
+struct matrix *matrix_get_cut_column_vector(struct matrix *mtrx, size_t column, size_t first_row);
+int matrix_vector_norm(struct matrix *vector, double _Complex *norm);
+struct matrix *matrix_housholder_vector(struct matrix *vector);
+struct matrix *matrix_housholder_left_mul(struct matrix *mtrx, struct matrix* housholder_vector);
+struct matrix *matrix_housholder_right_mul(struct matrix *mtrx, struct matrix* housholder_vector);
+void matrix_housholder_transformation(struct matrix *mtrx);
 void matrix_print(struct matrix *mtrx);
 void matrix_free(struct matrix *mtrx);
+
+
